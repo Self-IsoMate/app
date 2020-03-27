411f922bea10c7ec0659afe3dea167e6dc6397a9
Helen Evans <helenijevans@gmail.com>
2020-03-27 19:13:18 +0000

    added bar at bottom

--- app/components/App.vue
+++ app/components/App.vue
@@ -6,11 +6,15 @@
                 <Image src.decode="font://&#xf015;" class="fas t-36"></Image>
             </TabStripItem>
             <TabStripItem class="navigation__item">
-                <Label text="Browse"></Label>
+                <Label text="Challenges"></Label>
                 <Image src.decode="font://&#xf1ea;" class="far t-36"></Image>
             </TabStripItem>
             <TabStripItem class="navigation__item">
-                <Label text="Search"></Label>
+                <Label text="Community"></Label>
+                <Image src.decode="font://&#xf002;" class="fas t-36"></Image>
+            </TabStripItem>
+            <TabStripItem class="navigation__item">
+                <Label text="Chat"></Label>
                 <Image src.decode="font://&#xf002;" class="fas t-36"></Image>
             </TabStripItem>
         </TabStrip>
--- app/components/Items.vue
+++ app/components/Items.vue
@@ -1,7 +1,7 @@
 <template>
   <Page>
     <ActionBar>
-      <Label text="Home"></Label>
+      <Label text="Welcome!"></Label>
     </ActionBar>
 
     <ListView for="item in items" @itemTap="onItemTap">
