e13b8e679b23abb32ca250d19d5890e8126ae24d
Deppy Tsivoula Lazogka <deppy.t.l._@hotmail.com>
2020-03-28 19:13:44 +0200

    forgot password and register

---
+++ app/components/Forgot.vue
@@ -0,0 +1,66 @@
+<template>
+    <Page backgroundColor="black">
+        <ActionBar title="Home" />
+        <ScrollView>
+            <StackLayout class="home-panel">
+                <Image
+                    src="https://play.nativescript.org/dist/assets/img/NativeScript_logo.png" />
+                <Label text="SELF-ISOMATE" class="label-class"/>
+                <TextField v-model="email" hint="EMAIL"class="my-class" />
+                <Button text="SEND RECOVERY EMAIL" @tap="onButtonTap"class="button-class"/>
+                <Button text="<- BACK" @tap="onButtonTap" fontStyle="Aharoni"
+                    backgroundColor="black" color="#00ff92" fontSize="25"
+                    marginRight="150" marginBottom="15" marginTop="50" />
+            </StackLayout>
+        </ScrollView>
+    </Page>
+
+</template>
+
+<script>
+    export default {
+        name: "Forgot",
+        data: () => {
+            return {
+                email: ""
+            };
+        },
+        methods: {
+            onButtonTap() {
+                console.log("Button was pressed");
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+    .home-panel {
+        font-family: Aharoni;
+        width: 80%;
+    }
+
+    .label-class {
+        color: #00ff92;
+        font-size: 28;
+        text-align: center;
+        vertical-align: middle;
+        margin-top: 10;
+    }
+
+    .my-class {
+        background-color: pink;
+        color: #00ff92;
+        font-size: 20;
+        border-color: #00ff92;
+        border-width: 4;
+        margin-top: 40;
+    }
+
+    .button-class {
+        background-color: #00ff92;
+        color: black;
+        font-size: 22;
+        margin-bottom: 15;
+    }
+</style>
---
+++ app/components/Register.vue
@@ -0,0 +1,72 @@
+<template>
+    <Page backgroundColor="black">
+        <ActionBar title="Home2" />
+        <ScrollView>
+            <StackLayout class="home-panel">
+                <Image
+                    src="https://play.nativescript.org/dist/assets/img/NativeScript_logo.png" />
+                <Label text="REGISTER" class="label-class" />
+                <TextField v-model="username" hint="USERNAME" class="my-class" marginTop= "40"/>
+                <TextField v-model="password" hint="PASSWORD" class="my-class" />
+                <TextField v-model="confirmpassword" hint="CONFIRM PASSWORD" class="my-class" />
+                <TextField v-model="email" hint="EMAIL" class="my-class" />
+                <Button text="CONTINUE" @tap="onButtonTap" class="button-class" />
+            </StackLayout>
+        </ScrollView>
+    </Page>
+
+</template>
+
+<script>
+    export default {
+        name: "Register",
+        data: () => {
+            return {
+                message: "",
+                username: "",
+                password: "",
+                confirmpassword: "",
+                email: ""
+            };
+        },
+        methods: {
+            onButtonTap() {
+                console.log("Button was pressed");
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+    .home-panel {
+        font-family: aharoni;
+        width: 80%;
+    }
+
+    .label-class{
+            color: #00ff92;
+            font-size: 28;
+            text-align: center;
+            vertical-align: middle;
+            margin-top: 10;
+    }
+
+    .my-class {
+        background-color: pink;
+        color: #00ff92;
+        font-size: 20;
+        border-color: #00ff92;
+        border-width: 4;
+    }
+
+    .button-class {
+        background-color: #00ff92;
+        color: black;
+        font-size: 22;
+        margin-bottom: 15;
+        margin-top: 50;
+    }
+
+    
+</style>
