7d5924184bc3571dd02c6634fa96a9a6bc1e92b5
sophie-norman <s.norman2@newcastle.ac.uk>
2020-03-28 21:40:59 +0000

    linking pages to login

--- app/components/LoginMain.vue
+++ app/components/LoginMain.vue
@@ -8,9 +8,9 @@
 				<TextField v-model="password" hint="Password" secure="true"/>
 				<Button text="LOGIN" @tap="handleLogin($event)"></Button>
 				<GridLayout columns="*, *" rows="*, *">
-					<Button col="0" row="0" text="REGISTER" @tap="handleLogin($event)"></Button>
+					<Button col="0" row="0" text="REGISTER" @tap="navigateRegister"></Button>
 					<Button col="1" row="0" text="GUEST" @tap="handleLogin($event)"></Button>
-					<Label row="1" colSpan="2" text="Forgot password?" @tap="handleLogin($event)" class="forgot-pass"></Label>
+					<Label row="1" colSpan="2" text="Forgot password?" @tap="navigateForgot" class="forgot-pass"></Label>
 				</GridLayout>
 			</StackLayout>
 		</ScrollView>
@@ -18,8 +18,8 @@
 </template>
 
 <script>
-import Browse from './Browse.vue';
-import LoginQuestionsMentorVue from './LoginQuestionsMentor.vue';
+import Register from "./Register";
+import Forgot from "./Forgot";
 
 export default {
 	name: 'LoginMain',
@@ -31,8 +31,14 @@ export default {
         };
 	},
 	methods: {
+		navigateRegister(event) {
+			this.$navigateTo(Register);
+		},
+		navigateForgot(event) {
+			this.$navigateTo(Forgot);
+		},
 		handleLogin(event) {
-			this.$navigateTo(LoginQuestionsMentorVue);
+
 		}
 	}
 }
