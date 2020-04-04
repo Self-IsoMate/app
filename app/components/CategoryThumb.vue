bae39b6bec599967ceea3a5259a55239805ac51d
sophie-norman <s.norman2@newcastle.ac.uk>
2020-04-04 17:20:08 +0100

    fixing android manifest

---
+++ app/components/CategoryThumb.vue
@@ -0,0 +1,18 @@
+<template>
+	<StackLayout padding="5" backgroundColor="#E8E8E8" height="500">
+		<StackLayout class="postContainer">
+			<Image :src="category.image" marginTop="10" />
+			<Label :text="category.name" />
+		</StackLayout>
+	</StackLayout>
+</template>
+<script>
+export default {
+	props: {
+		category: {}
+	}
+}
+</script>
+<style scoped>
+
+</style>
