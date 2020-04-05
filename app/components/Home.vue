dbedd706dc462cdf92f27caaebb57662d9eeaaf5
sophie-norman <s.norman2@newcastle.ac.uk>
2020-04-05 17:57:30 +0100

    Refactoring home page & adding category

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,117 +1,35 @@
 <template>
     <Page class="page">
 
-        <ActionBar title="" class="action-bar header">
-            <StackLayout orientation="horizontal" height="38" alignItems="left"
-                class="actionBarContainer">
-                <StackLayout class="HLeft" style="margin-top:10;" @tap="toggleDrawer()">
-                    <Label :text="drawerToggle ? drawer2: drawer1" style="font-size:27;color:#fff;"
-                        class="font-awesome" />
-                </StackLayout>
-                <StackLayout class="HMid" alignItems="left">
-                    <TextField placeholderColor="white" id="searchField"
-                        editable="true" hint="      Search" returnKeyType="search"
-                        ios:height="30" ios:marginTop="3"
-                        android:paddingBottom="5" class="searchField font-awesome"
-                        color="#fff" />
-                </StackLayout>
-                <StackLayout class="HRight">
-
-                </StackLayout>
-            </StackLayout>
-        </ActionBar>
-
-        <RadSideDrawer ref="drawer" @drawerOpened="onDrawerOpened()"
-            @drawerClosed="onDrawerClosed()">
-            <StackLayout ~drawerContent backgroundColor="#eee">
-                <StackLayout height="2%"></StackLayout>
-                <StackLayout class="">
-                    <StackLayout class = "prof" @tap="profileTap()">
-                    <Label text="  Profile" paddingLeft="15%" color="black"
-                        class="drawerItemText font-awesome" margin="15" />
-                    </StackLayout>
-                    <StackLayout class = "notif" @tap="notificationTap()">
-                    <Label text="  Notifications" paddingLeft="15%" color="black"
-                        class="drawerItemText font-awesome" margin="15" />
-                    </StackLayout>
-                    <StackLayout class = "settings" @tap="settingsTap()">
-                    <Label text="  Settings" paddingLeft="15%" color="black"
-                        class="drawerItemText font-awesome" margin="15" />
-                    </StackLayout>
-                    <StackLayout class = "help" @tap="helpTap()">
-                    <Label text="  Help" paddingLeft="15%" color="black"
-                        class="drawerItemText font-awesome" margin="15" />
-                    </StackLayout>
-                    <StackLayout class = "logout" @tap="logOut()">
-                    <Label text="  Log out" paddingLeft="15%" color="black"
-                        class="drawerItemText font-awesome" margin="15" />
-                    </StackLayout>
-                </StackLayout>
-            </StackLayout>
-
+        <HeaderBar @toggleDrawer="toggleDrawer" :drawerToggle="drawerToggle" />
+        <RadSideDrawer ref="drawer" @drawerOpened="onDrawerOpened()" @drawerClosed="onDrawerClosed()">
+            
+            <SideBar ~drawerContent />
             <StackLayout ~mainContent>
-
                 <DockLayout>
+                    <!-- Actual page content goes here (in top) -->
 
-                    <ScrollView dock="top" scrollBarIndicatorVisible="true" height="90%">
-                        <StackLayout>
-                            <CategoryThumb v-for="category in categories" :key="category._id" :category="category" />
-                        </StackLayout>
+                    <ScrollView dock="top" scrollBarIndicatorVisible="true" height="90%" padding="0 10">
+                        <WrapLayout>
+                            <CategoryThumb v-for="category in categories" :key="category._id" :category="category" @tap="selectCategory" />
+                        </WrapLayout>
                     </ScrollView>
 
-                    <StackLayout dock="bottom" height="10%" style="border-color:#E8E8E8;border-width:1;background:#fff;">
-                        <StackLayout orientation="horizontal">
-                            <StackLayout class="navItem" @tap="homeTap()">
-                                <Label text="" android:class="notificationAndroid"
-                                    ios:class="notification" opacity="0" />
-                                <Label text="" :color="mainColor"
-                                    android:style="font-size:25;margin-top:-15"
-                                    ios:style="font-size:30;margin-top:-15"
-                                    class="font-awesome" />
-                            </StackLayout>
-                            <StackLayout class="navItem" @tap="competitionTap()">
-                                <Label text="" android:class="notificationAndroid"
-                                    ios:class="notification" opacity="0" />
-                                <Label text=" " android:style="font-size:23;margin-top:-15"
-                                    ios:style="font-size:29;margin-top:-15"
-                                    class="font-awesome" />
-                            </StackLayout>
-                            <StackLayout class="navItem" @tap="communityTap()">
-                                <Label text="" android:class="notificationAndroid"
-                                    ios:class="notification" opacity="0" />
-                                <Label text="" :color="profileColor"
-                                    android:style="font-size:25;margin-top:-15"
-                                    ios:style="font-size:30;margin-top:-15"
-                                    class="font-awesome" />
-                            </StackLayout>
-                            <StackLayout class="navItem" @tap="chatTap()">
-                                <Label text="" android:class="notificationAndroid"
-                                    ios:class="notification" opacity="0" />
-                                <Label text="" android:style="font-size:25;margin-top:-15"
-                                    ios:style="font-size:30;margin-top:-15"
-                                    class="font-awesome" />
-                            </StackLayout>
-                        </StackLayout>
-                    </StackLayout>
+                    <NavBar dock="bottom" height="10%" selectedtab="home" />
 
                 </DockLayout>
-
             </StackLayout>
         </RadSideDrawer>
 
     </page>
 </template>
 <script>
-    import Community from "./Community";
-    import Chat from "./Chat";
-    import Competitions from "./Competitions"
-    import Profile from "./Profile";
-    import Notifications from "./Notifications";
-    import Settings from "./Settings";
-    import Help from "./Help";
-    import LoginScreen from "./LoginMain";
+    import SideBar from "./SideBar";
+    import NavBar from "./NavBar";
     import CategoryThumb from "./CategoryThumb";
     import BackendService from "../services/BackendService";
+    import Category from "./Category";
+    import HeaderBar from "./HeaderBar";
 
     export default {
         data() {
@@ -124,7 +42,10 @@
             };
         },
         components: {
-            CategoryThumb
+            CategoryThumb,
+            SideBar,
+            NavBar,
+            HeaderBar
         },
         created () {
             let service = new BackendService();
@@ -150,57 +71,17 @@
             },
             toggleDrawer() {
                 this.$refs.drawer.nativeView.toggleDrawerState();
-            },
-            homeTap() {},
-            communityTap() {
-                this.$navigateTo(Community, {
-                    animated: false,
-                    clearHistory: true
-                });
-            },
-            chatTap() {
-                this.$navigateTo(Chat, {
-                    animated: false,
-                    clearHistory: true
-                });
-            },
-            competitionTap() {
-                this.$navigateTo(Competitions, {
-                    animated: false,
-                    clearHistory: true
-                });
-            },
-            profileTap() {
-                this.$navigateTo(Profile, {
-                    animated: false,
-                    clearHistory: true
-                });
-            },
-            notificationTap() {
-                this.$navigateTo(Notifications, {
-                    animated: false,
-                    clearHistory: true
-                });
-            },
-            settingsTap() {
-                this.$navigateTo(Settings, {
-                    animated: false,
-                    clearHistory: true
-                });
-            },
-            helpTap(){
-                this.$navigateTo(Help, {
-                    animated: false,
-                    clearHistory: true
-                });
-            },
-            logOut(){
-                this.$navigateTo(LoginScreen, {
-                    animated: false,
-                    clearHistory: true
-                });
             }, //put in here navigate to log-in screen
-            showDetails() {}
+            selectCategory(event) {
+                console.log(event);
+                this.$navigateTo(Category, {
+                    animated: false,
+                    clearHistory: true,
+                    props: {
+                        subcategories: event.category.subcategories
+                    }
+                })
+            }
         }
     };
 </script>
---
+++ app/components/SideBar.vue
@@ -0,0 +1,80 @@
+<template>
+	<StackLayout backgroundColor="#eee">
+		<StackLayout height="2%"></StackLayout>
+		<StackLayout>
+			<StackLayout class = "prof" @tap="profileTap()">
+			<Label text="  Profile" paddingLeft="15%" color="black"
+				class="drawerItemText font-awesome" margin="15" />
+			</StackLayout>
+			<StackLayout class = "notif" @tap="notificationTap()">
+			<Label text="  Notifications" paddingLeft="15%" color="black"
+				class="drawerItemText font-awesome" margin="15" />
+			</StackLayout>
+			<StackLayout class = "settings" @tap="settingsTap()">
+			<Label text="  Settings" paddingLeft="15%" color="black"
+				class="drawerItemText font-awesome" margin="15" />
+			</StackLayout>
+			<StackLayout class = "help" @tap="helpTap()">
+			<Label text="  Help" paddingLeft="15%" color="black"
+				class="drawerItemText font-awesome" margin="15" />
+			</StackLayout>
+			<StackLayout class = "logout" @tap="logOut()">
+			<Label text="  Log out" paddingLeft="15%" color="black"
+				class="drawerItemText font-awesome" margin="15" />
+			</StackLayout>
+		</StackLayout>
+	</StackLayout>
+</template>
+
+<script>
+import Community from "./Community";
+import Chat from "./Chat";
+import Competitions from "./Competitions"
+import Profile from "./Profile";
+import Notifications from "./Notifications";
+import Settings from "./Settings";
+import Help from "./Help";
+import LoginScreen from "./LoginMain";
+
+export default {
+	name: "SideBar",
+	components: {
+	},
+	methods: {
+            profileTap() {
+                this.$navigateTo(Profile, {
+                    animated: false,
+                    clearHistory: true
+                });
+            },
+            notificationTap() {
+                this.$navigateTo(Notifications, {
+                    animated: false,
+                    clearHistory: true
+                });
+            },
+            settingsTap() {
+                this.$navigateTo(Settings, {
+                    animated: false,
+                    clearHistory: true
+                });
+            },
+            helpTap(){
+                this.$navigateTo(Help, {
+                    animated: false,
+                    clearHistory: true
+                });
+            },
+            logOut(){
+                this.$navigateTo(LoginScreen, {
+                    animated: false,
+                    clearHistory: true
+                });
+            }
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
