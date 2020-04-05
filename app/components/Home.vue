d9d4647253f4c4b58841f44f9eeca97b1c694bdc
sophie-norman <s.norman2@newcastle.ac.uk>
2020-04-05 19:10:49 +0100

    Applying changes to Community and Competitions

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,13 +1,32 @@
 <template>
     <Page class="page">
 
-        <HeaderBar @toggleDrawer="toggleDrawer" :drawerToggle="drawerToggle" />
-        <RadSideDrawer ref="drawer" @drawerOpened="onDrawerOpened()" @drawerClosed="onDrawerClosed()">
+        <ActionBar title="" class="action-bar header">
+            <StackLayout orientation="horizontal" height="38" alignItems="left"
+                class="actionBarContainer">
+                <StackLayout class="HLeft" style="margin-top:10;" @tap="toggleDrawer">
+                    <Label :text="drawerToggle ? drawer2: drawer1" style="font-size:27;color:#fff;"
+                        class="font-awesome" />
+                </StackLayout>
+                <StackLayout class="HMid" alignItems="left">
+                    <TextField placeholderColor="white" id="searchField"
+                        editable="true" hint="      Search" returnKeyType="search"
+                        ios:height="30" ios:marginTop="3"
+                        android:paddingBottom="5" class="searchField font-awesome"
+                        color="#fff" />
+                </StackLayout>
+                <StackLayout class="HRight">
+
+                </StackLayout>
+            </StackLayout>
+        </ActionBar>
+
+        <RadSideDrawer ref="drawer" @drawerOpened="onDrawerOpened" @drawerClosed="onDrawerClosed">
             
             <SideBar ~drawerContent />
             <StackLayout ~mainContent>
                 <DockLayout>
-                    <!-- Actual page content goes here (in top) -->
+                    <!-- Actual page content goes here (in dock top) -->
 
                     <ScrollView dock="top" scrollBarIndicatorVisible="true" height="90%" padding="0 10">
                         <WrapLayout>
@@ -29,23 +48,20 @@
     import CategoryThumb from "./CategoryThumb";
     import BackendService from "../services/BackendService";
     import Category from "./Category";
-    import HeaderBar from "./HeaderBar";
 
     export default {
         data() {
             return {
                 drawerToggle: false,
+                categories: [],
                 drawer1: "",
-                drawer2: "",
-                mainColor: "#00ff92",
-                categories: []
+                drawer2: ""
             };
         },
         components: {
             CategoryThumb,
             SideBar,
-            NavBar,
-            HeaderBar
+            NavBar
         },
         created () {
             let service = new BackendService();
