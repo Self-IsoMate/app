b959063633fec084359a1170bb5db49e3be9d580
Helen Evans <helenijevans@gmail.com>
2020-04-07 16:41:25 +0100

    initial commit

--- app/components/ChatroomAdd.vue
+++ app/components/ChatroomAdd.vue
@@ -1,32 +1,28 @@
 <template>
     <Page class="page">
 
-
         <ActionBar title="" class="action-bar header">
             <StackLayout orientation="horizontal" height="38" alignItems="left"
                 class="actionBarContainer">
-                <StackLayout class="HLeft" style="margin-top:10;" @tap="toggleDrawer()">
-                    <Label :text="drawerToggle ? drawer2: drawer1" style="font-size:27;color:#fff;"
+                <StackLayout class="HLeft" style="margin-top:10;" @tap="chatTap()">
+                    <Label :text="back" style="font-size:27;color:#fff;"
                         class="font-awesome" />
                 </StackLayout>
                 <StackLayout class="HMid" alignItems="left">
                     <TextField placeholderColor="white" id="searchField"
-                        editable="true" hint="      Search" returnKeyType="search"
+                        editable="true" hint="      Search for a Chatroom" returnKeyType="search"
                         ios:height="30" ios:marginTop="3"
                         android:paddingBottom="5" class="searchField font-awesome"
                         color="#fff" />
                 </StackLayout>
-                <StackLayout class="HRight" @tap="addTap()">
-                    <Label text="+" style="font-size:40;color:#fff;" paddingLeft="15%"
-                        class="font-awesome" />
+                <StackLayout class="HRight">
                 </StackLayout>
             </StackLayout>
         </ActionBar>
 
-
         <RadSideDrawer ref="drawer" @drawerOpened="onDrawerOpened()"
             @drawerClosed="onDrawerClosed()">
-            <StackLayout ~drawerContent backgroundColor="#eee">
+                      <StackLayout ~drawerContent backgroundColor="#eee">
                 <StackLayout height="2%"></StackLayout>
                 <StackLayout class="">
                     <StackLayout class = "prof" @tap="profileTap()">
@@ -55,37 +51,11 @@
             <StackLayout ~mainContent>
 
                 <DockLayout>
+
                     <StackLayout dock="top" height="90%" width="100%" style="">
-
-
-                        <ListView for="item in conversations" :key="index"
-                            height="100%" separatorColor="transparent" id="listView">
-                            <v-template>
-
-                                <StackLayout orientation="horizontal" style="border-bottom-width:1;border-bottom-color:#E4E4E4;"
-                                    padding="10" @tap="chatroomTap(item.convFriendName)">
-                                    <StackLayout width="20%">
-                                        <Image :src="item.convFriendImg"
-                                            stretch="aspectFill" class="conImg" />
-                                    </StackLayout>
-                                    <StackLayout marginLeft="10" paddingTop="3"
-                                        width="50%">
-                                        <Label :text="item.convFriendName"
-                                            :class="'convFriendName ' + item.read" />
-                                        <Label :text="item.convText" :class="'convTextOut ' + item.read" />
-                                    </StackLayout>
-                                    <StackLayout marginLeft="10" paddingTop="3"
-                                        width="60%">
-                                        <Label :text="item.convDate" :class="'convDateOut ' + item.read" />
-                                        <Label text="" :visibility="item.seenVisibility"
-                                            style="font-size:17;text-align:center;margin-top:12;color:#1aa3ff;"
-                                            class="font-awesome" />
-                                    </StackLayout>
-                                </StackLayout>
-
-                            </v-template>
-                        </ListView>
-
+                        <Button text="Popular"/>
+                        <Button text="Recommended for you"/>
+                        <Button text="All Chatrooms"/>
                     </StackLayout>
 
                     <StackLayout dock="bottom" height="10%" style="border-color:#E4E4E4;border-width:1;background:#fff;">
@@ -122,15 +92,18 @@
                             </StackLayout>
                         </StackLayout>
                     </StackLayout>
+
                 </DockLayout>
 
             </StackLayout>
         </RadSideDrawer>
-    </Page>
+
+    </page>
 </template>
 
 <script>
     import Home from "./Home";
+    import Chat from "./Chat";
     import Community from "./Community";
     import Competitions from "./Competitions"
     import Profile from "./Profile";
@@ -138,45 +111,19 @@
     import Settings from "./Settings";
     import Help from "./Help";
     import LoginScreen from "./LoginMain";
-    import Chatroom from "./Chatroom";
-    import ChatroomAdd from "./ChatroomAdd";
 
-    export default {
+     export default {
         created() {},
         data() {
             return {
+                back:"",
                 drawerToggle: false,
                 drawer1: "",
                 drawer2: "",
                 mainColor: "#00ff92",
-                conversations: [{
-                        convFriendImg: "~/assets/images/violinGroup.png",
-                        read: "notRead",
-                        convFriendName: "#ViolinIntermediate",
-                        convText: "Lindsay21: can anyone help me with my double stops?",
-                        convDate: "19:01",
-                        seenVisibility: "collapse"
-                    },
-                    {
-                        convFriendImg: "~/assets/images/uk.png",
-                        read: "notRead",
-                        convFriendName: "#UnitedKingdom",
-                        convText: "DiscoDan: Honestly not surprised Boris has it",
-                        convDate: "18:43",
-                        seenVisibility: "collapse"
-                    },
-                    {
-                        convFriendImg: "~/assets/images/musicCollaboration.jpeg",
-                        read: "read",
-                        convFriendName: "#MusicCollaborate",
-                        convText: "You: Any guitarists out there? Need background instrumentals for my new track",
-                        convDate: "2 minutes ago",
-                        seenVisibility: "visible"
-                    }
-                ]
             };
         },
-        methods: {
+         methods: {
             onDrawerClosed() {
                 this.drawerToggle = false;
             },
@@ -187,7 +134,7 @@
                 this.$refs.drawer.nativeView.toggleDrawerState();
             },
             homeTap() {
-                 this.$navigateTo(Home, {
+                this.$navigateTo(Home, {
                     animated: false,
                     clearHistory: true
                 });
@@ -198,7 +145,12 @@
                     clearHistory: true
                 });
             },
-            chatTap(){},
+            chatTap() {
+                this.$navigateTo(Chat, {
+                    animated: false,
+                    clearHistory: true
+                });
+            },
             competitionTap() {
                 this.$navigateTo(Competitions, {
                     animated: false,
@@ -233,22 +185,9 @@
                 this.$navigateTo(LoginScreen, {
                     animated: false,
                     clearHistory: true
-                }); 
+                });
             }, //put in here navigate to log-in screen
-            showDetails() {},
-            chatroomTap(name){
-                this.$navigateTo(Chatroom, {
-                    props: {chatName: name},
-                    animated: false,
-                    clearHistory: true
-                }); 
-            },
-            addTap(){
-                 this.$navigateTo(ChatroomAdd, {
-                    animated: false,
-                    clearHistory: true
-                }); 
-            }
+            showDetails() {}
         }
     };
 </script>
